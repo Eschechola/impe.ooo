54f73f2023d0e4b80210c33e1868170c8b09218a
Lucas Eschechola <lucas.eschechola@outlook.com>
2022-03-04 01:12:06 -0300

    Add body-parser package

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-// import swaggerUi from "swagger-ui-express";
-// import swaggerJsDoc from "swagger-jsdoc";
 import router from './routes/router';
 
 dotenv.config()
@@ -18,25 +16,6 @@ app.use(router);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-// const opts = {
-//     definition: {
-// 		openapi: "3.0.0",
-// 		info: {
-// 			title: "Library API",
-// 			version: "1.0.0",
-// 			description: "A simple Express Library API",
-// 		},
-// 		servers: [
-// 			{
-// 				url: "http://localhost:4000",
-// 			},
-// 		],
-// 	},
-// 	apis: ["./routes/*.js"],
-// };
-// const swaggerDocument = swaggerJsDoc(opts);
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { verifyToken } from '../middlewares/authentication.middleware';
 
 import WordController from "../controllers/word.controller";
 
 const router = express.Router();
+const jsonParser = bodyParser.json();
 const wordController = new WordController();
 
-//router.use('/api/word/get-word', verifyToken);
-router.get('/api/word/get-word', wordController.getWord);
+router.get('/api/word/get', jsonParser, wordController.getWord);
+
+router.use('/api/word/create', verifyToken);
+router.post('/api/word/create', jsonParser, wordController.createWord);
 
 export default router;
